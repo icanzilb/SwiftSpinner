d98fb0cc95bb1c969280d4f1edb448f587a67ee1
Doug Woodgate <douglas.woodgate@gmail.com>
2016-02-19 16:14:37 -0600

    Adding Carthage Support

---
+++ SwiftSpinner/SwiftSpinner.h
@@ -0,0 +1,19 @@
+//
+//  SwiftSpinner.h
+//  SwiftSpinner
+//
+//  Created by Doug Woodgate (GE) on 2/19/16.
+//  Copyright © 2016 Dwoodgate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftSpinner.
+FOUNDATION_EXPORT double SwiftSpinnerVersionNumber;
+
+//! Project version string for SwiftSpinner.
+FOUNDATION_EXPORT const unsigned char SwiftSpinnerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftSpinner/PublicHeader.h>
+
+

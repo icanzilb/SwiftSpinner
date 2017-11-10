ee0e444c754ee86579244685a8007f41e0de4f48
Marin Todorov <marin@underplot.com>
2017-11-10 22:15:30 +0100

    cleaned up demo app folder

--- DemoApp/Pods/Target Support Files/Pods-SwiftSpinnerDemo/Pods-SwiftSpinnerDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftSpinnerDemo : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftSpinnerDemo
-@end
--- DemoApp/Pods/Target Support Files/SwiftSpinner/SwiftSpinner-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SwiftSpinner.h"
-
-FOUNDATION_EXPORT double SwiftSpinnerVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftSpinnerVersionString[];
-
